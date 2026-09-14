5390fbffa164f8c0ebb86b6792a8ef3dc2057c2f
michelldahiana <michellangulolondono@gmail.com>
2026-09-14 07:18:10 -0500

    feat: agregar operacion de modulo

--- calculadora.py
+++ calculadora.py
@@ -3,13 +3,22 @@
 def sumar(a, b):
     return a + b
 
+
 def restar(a, b):
     return a - b
 
+
 def multiplicar(a, b):
     return a * b
+
 
 def dividir(a, b):
     if b == 0:
         return "Error: no se puede dividir entre cero"
     return a / b
+
+
+def modulo(a, b):
+    if b == 0:
+        return "Error: no se puede calcular el modulo con cero"
+    return a % b
